a0fd625a36e35af1e14ac247c0d94aceb5a81329
Mayan Annie Rothstein <mayan@bu.edu>
2019-06-18 14:30:46 -0700

    set up schemas in routes/api folder

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
-const express = require("express")
+const express = require("express");
+var mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+var path = require('path');
 
 const app = express();
+//connect Database
+mongoose.connect('mongodb://localhost/soc_network', { useNewUrlParser: true });
+require('./config/mongoose.js');
 
-app.get('/', (req, res) => res.send('API Running'))
+//init Middleware
+app.use(express.json({extend: false}));
+
+app.get('/', (req, res) => res.send('API Running'));
+
+//define routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 //defaults to 5000
 const PORT = process.env.PORT || 5000;
