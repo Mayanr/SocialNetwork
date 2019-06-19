bc2aa405581d7bfc33696daa83f659705adabdab
Mayan Annie Rothstein <mayan@bu.edu>
2019-06-19 16:20:05 -0700

    Alerts for login and registration appear when invalid info is entered and disappear after a few seconds

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,8 +1,12 @@
 import React, { Fragment, useState} from 'react';
-import axios from "axios";
+// import axios from "axios";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { setAlert } from "../../actions/alert";
+import PropTypes from "prop-types"
 
-const Register = () => {
+
+const Register = ({setAlert}) => {
     const [formData, setFormData ] = useState({
         name: "",
         email: "",
@@ -16,7 +20,7 @@ const Register = () => {
     const onSubmit = async e => {
         e.preventDefault();
         if(password !== password2){
-            console.log("passwords do not match")
+            setAlert("Passwords do not match", "danger");
         } else {
           console.log("success");
             // const newUser = {
@@ -86,4 +90,7 @@ const Register = () => {
     )
 }
 
-export default Register
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired
+}
+export default connect(null, {setAlert})(Register);
